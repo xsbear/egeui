3176cdde0c6ba81df501d572a815006a65901d83
xsbear <xsbear@gmail.com>
2014-06-25 23:18:49 +0800

    implement inherit mechanism

--- src/egeui.js
+++ src/egeui.js
@@ -98,6 +98,40 @@
         };
 
 
+        var Widget = function() {}
+        Widget.extend = function(protoProps, staticProps) {
+            var parent = this;
+            var child;
+
+            // The constructor function for the new subclass is either defined by you
+            // (the "constructor" property in your `extend` definition), or defaulted
+            // by us to simply call the parent's constructor.
+            if (protoProps && protoProps.hasOwnProperty('constructor')) {
+                child = protoProps.constructor;
+            } else {
+              child = function(){ return parent.apply(this, arguments); };
+            }
+
+            // Add static properties to the constructor function, if supplied.
+            $.extend(child, parent, staticProps);
+
+            // Set the prototype chain to inherit from `parent`, without calling
+            // `parent`'s constructor function.
+            var Surrogate = function(){ this.constructor = child; };
+            Surrogate.prototype = parent.prototype;
+            child.prototype = new Surrogate();
+
+            // Add prototype properties (instance properties) to the subclass,
+            // if supplied.
+            if (protoProps) $.extend(child.prototype, protoProps);
+
+            // Set a convenience property in case the parent's prototype is needed
+            // later.
+            child.superClass = parent.prototype;
+
+            return child;
+        };
+
 
 
         var isIE6 = !window.XMLHttpRequest
@@ -178,23 +212,77 @@
 
         /* Overlay CLASS DEFINITION
          * ====================== */
-        var Overlay = function(options) {
-            var defaults = {
-                // element, template, width, height, id , className, trigger
-                parentNode: document.body,
-                position: 'absolute',
-                top: '-9999px',
-                left: '-9999px',
-                zIndex: 99,
-                visible: false,
-                hideBlur: false,
-                align: ''
-            }
-            this.options = $.extend(defaults, options);
-            if(this.options.visible){
-                this.init()
-            }
-        }
+        var Overlay = Widget.extend({
+            constructor: function(options) {
+                var defaults = {
+                    // element, template, width, height, id , className, trigger
+                    parentNode: document.body,
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    zIndex: 99,
+                    visible: false,
+                    hideBlur: false,
+                    align: ''
+                }
+                this.options = $.extend(defaults, options);
+                if(this.options.visible){
+                    this.init()
+                }
+            },
+            init: function() {
+                var options = this.options;
+                var parentNode = $$(options.parentNode);
+                this.$element = $$(options.element || options.template);
+                this.element = this.$element[0];
+                options.id && this.$element.attr('id', options.id);
+                options.className && this.$element.addClass(options.className)
+                this.$element.css({
+                    'position': options.position,
+                    'z-index': options.zIndex,
+                    'top': options.top,
+                    'left': options.left,
+                    'display': 'none'
+                });
+                options.width && this.$element.css('width', options.width);
+                options.height && this.$element.css('height', options.height);
+                if(!$.contains(document.documentElement, this.element)){
+                    this.$element.appendTo(parentNode);
+                }
+
+                if(options.align){
+                    var align = options.align;
+                    if(align.elem && align.pos){
+                        Position.pin(this.$element, align)
+                    } else {
+                        Position.center(this.$element, align)
+                    }
+                } else {
+                    Position.center(this.$element)
+                }
+
+                options.visible && this.$element.show();
+
+                if(options.hideBlur){
+                    Overlay.blurOverlays.push(this);
+                    this._relativeElements.push(this.element)
+                }
+            },
+            show : function() {
+                if(!this.$element){
+                    this.init()
+                }
+                this.$element.show();
+            },
+            hide : function() {
+                this.$element.hide();
+            },
+            destroy : function() {
+                this.$element.remove();
+                erase(this, Overlay.blurOverlays)
+            },
+            _relativeElements: []
+        });
 
         // 绑定 blur 隐藏事件
         Overlay.blurOverlays = [];
@@ -202,66 +290,16 @@
             hideBlurOverlays(e);
         });
 
-        Overlay.prototype = {
-            // constructor: Overlay,
-            init: function() {
-                var options = this.options;
-                var parentNode = $$(options.parentNode);
-                this.layer = $$(options.element || options.template);
-                options.id && this.layer.attr('id', options.id);
-                options.className && this.layer.addClass(options.className)
-                this.layer.css({
-                    'position': options.position,
-                    'z-index': options.zIndex,
-                    'top': options.top,
-                    'left': options.left,
-                    'display': options.visible ? 'block' : 'none'
-                });
-                options.width && this.layer.css('width', options.width);
-                options.height && this.layer.css('height', options.height);
-                this.layer.appendTo(parentNode);
-
-                if(options.align){
-                    var align = options.align;
-                    if(align.elem && align.pos){
-                        Position.pin(this.layer, align)
-                    } else {
-                        Position.center(this.layer, align)
-                    }
-                } else {
-                    Position.center(this.layer)
-                }
-
-                if(options.hideBlur){
-                    Overlay.blurOverlays.push(this);
-                }
-                if(options.trigger){
-                    this.triggerElement = options.trigger[0] || options.trigger;
-                }
-                this.element = this.layer[0];
-            },
-            show : function() {
-                if(!this.layer){
-                    this.init()
-                }
-                this.layer.show();
-            },
-            hide : function() {
-                this.layer.hide();
-            },
-            destroy : function() {
-                this.layer.remove();
-                erase(this, Overlay.blurOverlays)
-            }
-        }
-
         // hide blur overlays
         function hideBlurOverlays(e){
             $(Overlay.blurOverlays).each(function (index, item) {
-                if (!item || item.layer.is(':hidden')) {
+                if (!item || item.$element.is(':hidden')) {
                     return;
                 }
-                if (item.triggerElement === e.target || item.element === e.target || $.contains(item.element, e.target)) {
+                for (var i = 0; i < item._relativeElements.length; i++) {
+                    var el = item._relativeElements[i];
+                };
+                if (el === e.target || $.contains(el, e.target)) {
                     return;
                 }
                 item.hide();
@@ -278,186 +316,40 @@
         }
 
 
-        Overlay.extend = function(protoProps, staticProps) {
-            var parent = this;
-            var child;
+        /* Popup CLASS DEFINITION
+         * ====================== */
+        var Popup = Overlay.extend({
+            constructor: function(){
+                Popup.superClass.constructor.apply(this, arguments);
 
-            // The constructor function for the new subclass is either defined by you
-            // (the "constructor" property in your `extend` definition), or defaulted
-            // by us to simply call the parent's constructor.
-            if (protoProps && protoProps.hasOwnProperty('constructor')) {
-                child = protoProps.constructor;
-            } else {
-              child = function(){ return parent.apply(this, arguments); };
-            }
+                this.options.triggerType = this.options.triggerType || 'hover';
+                this._bindTrigger();
+            },
+            _bindTrigger: function(){
+                var triggerType = this.options.triggerType;
+                if(triggerType === 'click'){
+                    this._bindClick();
+                } else {
+                    this._bindHover();
+                }
+            },
 
-            // Add static properties to the constructor function, if supplied.
-            $.extend(child, parent, staticProps);
+            _bindClick: function(){
 
-            // Set the prototype chain to inherit from `parent`, without calling
-            // `parent`'s constructor function.
-            var Surrogate = function(){ this.constructor = child; };
-            Surrogate.prototype = parent.prototype;
-            child.prototype = new Surrogate();
-
-            // Add prototype properties (instance properties) to the subclass,
-            // if supplied.
-            if (protoProps) $.extend(child.prototype, protoProps);
-
-            // Set a convenience property in case the parent's prototype is needed
-            // later.
-            child.__super__ = parent.prototype;
-
-            return child;
-        };
-
-
-        var Menu = Overlay.extend({
-            showMenu: function(){
-                console.log('this is a menu')
             }
         })
 
-        /* Popup CLASS DEFINITION
-         * ====================== */
-        var Popup = function(element, options) {
-            this.options = options;
-            this.$element = $(element);
-        }
 
-        Popup.prototype = {
-            constructor : Popup,
-            overlay : null,
-            init : function() {
-                var o = this.options, $this = this;
 
-                if (o.modal) {
-                    this.overlay = new Overlay({zIndex:  o.zIndex - 1});
-                }
 
-                var _popup = $('<div class="mm-popup"></div>').appendTo('body').addClass(o.themeClass).css('z-index', o.zIndex);
+        var Menu = Overlay.extend({
+            init: function(){
+                this.options.element = this.options.element || this.options.template || '<ul class="egeui-menu"></ul>';
 
-                if (o.title !== '') {
-                    var _head = $('<div class="hd"><span class="title">' + o.title + '</span></div>').appendTo(_popup);
-                    if(o.closeIcon){
-                        $('<span class="close" title="关闭">&times;</span>').appendTo(_head).click(function() {
-                            if(o.closeMode === 'destroy'){
-                                $this.destroy()
-                            } else {
-                                $this.hide();
-                            }
-                        });
-                    }
-                }
-                var _body = this.$element.appendTo(_popup).addClass('bd');
-
-                if (o.buttons) {
-                    var _foot = $('<div class="ft"></div>').appendTo(_popup);
-
-                    if (!$.isArray(o.buttons)) {
-                        o.buttons = new Array(o.buttons);
-                    }
-                    for (var i = 0; i < o.buttons.length; i++) {
-                        var iBtn = o.buttons[i];
-                        oBtns = $.extend({
-                            classname: 'confirm',
-                            trigger: 'click'
-                        }, iBtn)
-                        if (!iBtn.hasOwnProperty('text')) {
-                            for (var key in iBtn) {
-                                oBtns.text = key;
-                                oBtns.handler = iBtn[key];
-                            }
-                        }
-                        if ( typeof (oBtns.handler) === 'string' && oBtns.handler === 'close') {
-                            $('<span class="button cancel-button">' + oBtns.text + '</span>').appendTo(_foot).click(function() {
-                                if(o.closeMode === 'destroy'){
-                                    $this.destroy()
-                                } else {
-                                    $this.hide();
-                                }
-                            });
-                        } else {
-                            $('<span class="button ' + oBtns.classname + '-button">' + oBtns.text + '</span>').appendTo(_foot).on(oBtns.trigger, oBtns.handler);
-                        }
-                    }
-                }
-                // if specify width option, then set the width of targe element
-                if (o.width) {
-                    // o.width = this.$element.outerWidth();
-                    this.$element.css('width', o.width);
-                }
-                // if specify height option, then set the height of targe element
-                if (o.height) {
-                    // o.height = this.$element.outerHeight() + ( _head ? _head.outerHeight() : 0) + ( _foot ? _foot.outerHeight() : 0);
-                    this.$element.css('height', o.height);
-                }
-
-                if(this.options.pin){
-                    Position.pin(_popup, this.options.pin)
-                } else {
-                    Position.center(_popup);
-                }
-
-                this.popuper = _popup;
-            },
-            show : function(options) {
-                this.popuper.show();
-                if (this.options.modal) {
-                    this.overlay.show();
-                }
-                this.popuper.show();
-            },
-            hide : function() {
-                this.popuper.hide();
-                if (this.options.modal) {
-                    this.overlay.hide();
-                }
-                if(this.options.onHide){
-                    this.options.onHide.call(this);
-                }
-            },
-            destroy : function() {
-                if(this.options.beforeClose){
-                    this.options.beforeClose();
-                }
-                this.popuper.remove();
-                if (this.options.modal) {
-                    this.overlay.destroy();
-                }
-                this.$element.removeData('popup');
-                if(this.options.onDestroy){
-                    this.options.onDestroy.call(this);
-                }
-            },
-            rePosition: function(){
-                Position.center(this.popuper);
+                Menu.superClass.init.call(this);
             }
-        }
+        })
 
-        /* Popup PLUGIN DEFINITION
-         * ======================= */
-        $.fn.Popup = function(option) {
-            return this.each(function() {
-                var $this = $(this), data = $this.data('popup'), options = $.extend({}, $.fn.Popup.defaults, $this.data(), typeof option === 'object' && option)
-                if (!data)
-                    $this.data('popup', ( data = new Popup(this, options)))
-                if ( typeof option === 'string')
-                    data[option]()
-                else if (options.show)
-                    data.init()
-            })
-        }
-
-        $.fn.Popup.defaults = {
-            show : true,
-            modal : false,
-            themeClass : 'mmpop-aero',
-            title : '',
-            closeIcon: true,
-            zIndex: 500,
-            closeMode: 'destroy'
-        }
 
 
     var pub = {};
